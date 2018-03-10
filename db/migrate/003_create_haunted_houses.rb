351dee9ceba11fb578f9ea8c832cc25789a9476b
Chris Henry <cdhenry@gmail.com>
2018-03-10 14:22:49 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,10 +1,22 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
-    create_table :costumes do |t|
+    create_table :haunted_houses do |t|
       t.string :name
+      t.text :location
+      t.string :theme
       t.float :price
-      t.integer :size
-      t.text :image_url
+      t.boolean :family_friendly
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
+
+name
+location
+theme
+price
+:family_friendly or not
+opening date
+closing date
+long description

0a1c38f347031d963aabc4423594d36e6d6f9c6a
Chris Henry <cdhenry@gmail.com>
2018-03-10 14:24:43 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,8 +3,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |t|
       t.string :name
       t.text :location
-      t.integer :number_of_costumes
-      t.integer :number_of_employees
+      t.integer :costume_inventory
+      t.integer :employees_count
       t.boolean :in_business
       t.datetime :opening_time
       t.datetime :closing_time

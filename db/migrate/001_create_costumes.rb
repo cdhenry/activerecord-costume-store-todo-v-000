44a7a6a87ea29cd38518fb0cdd56be8d3f1d1782
Chris Henry <cdhenry@gmail.com>
2018-03-10 14:25:42 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.string :name
       t.float :price
       t.integer :size
-      t.text :image_url
+      t.string :image_url
     end
   end
 end

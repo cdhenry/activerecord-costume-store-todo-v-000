d09dfd98e572d7e202ac427506013683084962c9
Chris Henry <cdhenry@gmail.com>
2018-03-10 14:19:14 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,10 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.integer :size
+      t.text :image_url
+    end
+  end
+end
